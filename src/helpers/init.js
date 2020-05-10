fcde4efe80b5b9e4df22597a7ca879e756213570
John Claro <jkrclaro@gmail.com>
2020-05-10 23:11:52 +0100

    Fix initialisation positions of pieces

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -9,31 +9,31 @@ import Rook from '../pieces/Rook';
 export default function initBoard() {
     const squares = Array(120).fill(null);
 
-    for (let index = 37; index < 45; index++) {
+    for (let index = 31; index < 39; index++) {
         squares[index] = new Pawn(2);
-        squares[index + 60] = new Pawn(1);
+        squares[index + 50] = new Pawn(1);
     }
 
-    squares[25] = new Rook(2);
-    squares[32] = new Rook(2);
-    squares[116] = new Rook(1);
-    squares[109] = new Rook(1);
+    squares[21] = new Rook(2);
+    squares[28] = new Rook(2);
+    squares[91] = new Rook(1);
+    squares[98] = new Rook(1);
 
-    squares[26] = new Knight(2);
-    squares[31] = new Knight(2);
-    squares[115] = new Knight(1);
-    squares[110] = new Knight(1);
+    squares[22] = new Knight(2);
+    squares[27] = new Knight(2);
+    squares[92] = new Knight(1);
+    squares[97] = new Knight(1);
 
-    squares[27] = new Bishop(2);
-    squares[30] = new Bishop(2);
-    squares[114] = new Bishop(1);
-    squares[111] = new Bishop(1);
+    squares[23] = new Bishop(2);
+    squares[26] = new Bishop(2);
+    squares[93] = new Bishop(1);
+    squares[96] = new Bishop(1);
 
-    squares[28] = new Queen(2);
-    squares[29] = new King(2);
+    squares[24] = new Queen(2);
+    squares[25] = new King(2);
 
-    squares[112] = new Queen(1);
-    squares[113] = new King(1);
+    squares[94] = new Queen(1);
+    squares[95] = new King(1);
 
     return squares;
 }
