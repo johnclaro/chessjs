5cd1e5fc30863d0c944f2f61c69fb4e8b8b1a244
John Claro <jkrclaro@gmail.com>
2020-05-11 02:52:42 +0100

    Do not render offboard squares

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,6 +5,20 @@ import Square from './Square';
 
 class Board extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            offboard: [
+                0,1,2,3,4,5,6,7,8,9,
+                10,11,12,13,14,15,16,17,18,19,
+                100,101,102,103,104,105,106,107,108,109,
+                110,111,112,113,114,115,116,117,118,119,
+                20,30,40,50,60,70,80,90,
+                29,39,49,59,69,79,89,99
+            ]
+        }
+    }
+
     renderSquare(index, shade) {
         return <Square
             key={index}
@@ -23,8 +37,13 @@ class Board extends React.Component {
         for (let row = 0; row < ROWS; row++) {
             const rows = [];
             for (let column = 0; column < COLUMNS; column++) {
-                const shade = (isEven(row) && isEven(column)) || (!isEven(row) && !isEven(column)) ? 'light-square' : 'dark-square';
+                const isEvenIndex = isEven(row) && isEven(column)
+                const isOddIndex = !isEven(row) && !isEven(column)
+                let shade = (isEvenIndex || (isOddIndex)) ? 'light-square' : 'dark-square';
                 const index = (row * COLUMNS) + column;
+                if (this.state.offboard.includes(index)) {
+                    shade = 'offboard-square';
+                }
                 rows.push(this.renderSquare(index, shade))
             }
             board.push(<div key={row} className='board-row'>{rows}</div>)
